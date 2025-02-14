3f2a362de1e074a088151790a6ebe1366835acf6
RPAAnubhav <sharmaanubhav2011@gmail.com>
2025-02-14 03:28:28 -0800

    Update DemoController.java

--- src/main/java/com/demo/dockerdemo/DemoController.java
+++ src/main/java/com/demo/dockerdemo/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
 	@GetMapping(value = "")
 	public String helloWorld() {
-		String s = "hello leo, alsi kutte!!>>>> NEW UPDATE APPENDED!!!";
+		String s = "hello leo, alsi kutte!!>>>> NEW UPDATE APPENDED!!!>>>Day2";
 		return s;
 	}
 }
